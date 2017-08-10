51a52c6520f17efa3a65abe41123cc82f69e69c9
vkefallinos <vkefallinos@gmail.com>
2017-08-11 01:56:36 +0300

    move to env variables

--- docker/common/nginx/launch.sh
+++ docker/common/nginx/launch.sh
@@ -1,7 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-rm /etc/nginx/conf.d/default.conf || :
-envsubst < auth.conf > /etc/nginx/conf.d/nginx.conf
-envsubst < auth.htpasswd > /etc/nginx/auth.htpasswd
 
+function ReplaceEnvironmentVariable() {
+    grep -rl "\$ENV{\"$1\"}" $3|xargs -r \
+        sed -i "s|\\\$ENV{\"$1\"}|$2|g"
+}
+cat /etc/nginx/conf.d/default.conf
+
+if [ -n "$DEBUG" ]; then
+    echo "Environment variables:"
+    env
+    echo "..."
+fi
+
+# Replace all variables
+for _curVar in `env | awk -F = '{print $1}'`;do
+    # awk has split them by the equals sign
+    # Pass the name and value to our function
+    ReplaceEnvironmentVariable ${_curVar} ${!_curVar} /etc/nginx/conf.d/default.conf
+    ReplaceEnvironmentVariable ${_curVar} ${!_curVar} /etc/nginx/users.htpasswd
+done
+
+cat /etc/nginx/conf.d/default.conf
 nginx -g "daemon off;"
